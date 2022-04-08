3c47555c04e6c86104f8f61223a61b7e3bfc1b31
VWcoder <antonio.konta.face@gmail.com>
2022-04-08 12:46:02 +0200

    Services

--- First-Project/src/app/recipes/recipe.service.ts
+++ First-Project/src/app/recipes/recipe.service.ts
@@ -1,8 +1,9 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.module";
 
-
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     
@@ -20,8 +21,15 @@ export class RecipeService {
     ])
   ];
 
+  constructor(private slService: ShoppingListService) {
+      this.slService;
+  }
+
  getRecipes() {
      return this.recipes.slice();
  }
 
+ addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+ }
 }
