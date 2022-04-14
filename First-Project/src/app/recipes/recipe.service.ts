184c240faf9eb9621aaa771a076874fa87cd9683
VWcoder <antonio.konta.face@gmail.com>
2022-04-14 15:52:16 +0200

    Observables - Subject

--- First-Project/src/app/recipes/recipe.service.ts
+++ First-Project/src/app/recipes/recipe.service.ts
@@ -1,11 +1,10 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import {  Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.module";
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
     
  private recipes: Recipe[] = [
     new Recipe('Schnitzel','Tasty and awesome', 'https://familynutrition.co.uk/wp-content/uploads/2014/06/Recipe-icon.png',[
