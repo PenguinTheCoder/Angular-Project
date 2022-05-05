74b1ef297b6b54d5d0546f173521b0a08f44d8e7
VWcoder <antonio.konta.face@gmail.com>
2022-05-05 15:52:13 +0200

    SignUp and Login requests

--- First-Project/src/app/auth/auth.component.ts
+++ First-Project/src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { AuthService } from "./auth.service";
+import { Observable } from "rxjs";
+import { AuthService, AuthResponseData } from "./auth.service";
 
 @Component ({
     selector: 'app-auth',
@@ -9,6 +10,8 @@ import { AuthService } from "./auth.service";
 
 export class AuthComponent {
     isLoginMode = true;
+    isLoading = false;
+    error: string = null;
 
     constructor(private authService: AuthService) {}
 
@@ -24,17 +27,25 @@ onSubmit(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
 
-    if(this.isLoginMode) {
+    let authObs: Observable<AuthResponseData>
 
+    this.isLoading = true;
+    if(this.isLoginMode) {
+       authObs = this.authService.login(email,password)
     } else {
-        this.authService.signUp(email, password).subscribe(
+       authObs = this.authService.signUp(email, password)
+    }
+
+    authObs.subscribe(
         resData => {
-        console.log(resData);    
-    }, error => {
-        console.log(error);      
-    }
+            console.log(resData);  
+            this.isLoading = false;  
+        }, errorMessage => {
+            console.log(errorMessage);
+            this.error = errorMessage;
+            this.isLoading = false;      
+        }
     )
-    }
 
     form.reset(); 
 }
